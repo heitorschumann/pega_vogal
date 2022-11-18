9207b4992c8151f8c9084329b9433e4023f65ecf
Heitor Schumann <heitorschumann@MacBook-Pro-de-Heitor.local>
2022-11-18 10:58:49 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [inputValue, setInputValue] = useState();
+	let [countState, setCountState] = useState(0);
+	let count = 0;
+
+	function handleChange(e) {
+		setInputValue(e.target.value);
+	}
+
+	function handleClick(e) {
+		e.preventDefault();
+		count = 0;
+		let subStr = inputValue.split("");
+		const vogais = ["a", "e", "i", "o", "u"];
+
+		subStr.forEach((l) => {
+			console.log(subStr);
+			console.log(l);
+			if (vogais.indexOf(l) !== -1) {
+				count++;
+			}
+			console.log(count);
+		});
+
+		setCountState(count);
+	}
+
+	return (
+		<>
+			<form className="formuto">
+				<input
+					type="text"
+					className="inputo"
+					value={inputValue}
+					onChange={handleChange}
+				/>
+				<button type="submit" onClick={handleClick} className="butto">
+					contar vogais
+				</button>
+			</form>
+			<span className="sputo">O número de vogais é: {countState}</span>
+		</>
+	);
 }
 
 export default App;
